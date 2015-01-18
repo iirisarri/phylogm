d429cc645c0d5c2e64134bfa19dce7ba01b4255d
iirisarri <irisarri.iker@gmail.com>
2015-01-18 18:00:02 +0100

    modified output to directly print bash commands

--- gene_jackknifing.pl
+++ gene_jackknifing.pl
@@ -7,7 +7,7 @@ use Getopt::Long;
 # Iker Irisarri, January 2015, University of Konstanz
 # This script takes a file with gene file names and subsamples it without replacement (gene jackknifing)
 # the number (-n) and size (-s) of the replicates should be given in the command line (integers)
-
+# bash commands to sort files into new folders, ready for concatenation
 
 my $usage = "gene_jackknifing.pl -f list_of_gene_files -s size_of_replicate -n number of replicates > out\n";
 my $genes_in; 
@@ -57,7 +57,48 @@ for (my $i=0; $i<$num_replicates; $i++) {
 
     }
 
-    print "replicate number $i:\n";
-    print join ("\n", @replicate), "\n\n";
+    #add folder name to each array element and sort
+    foreach my $elem ( @replicate ) {
+	$elem = $elem . " replicate.\$i";
+    }
+
+    my @replicate_sorted = sort @replicate;
+
+    print "i=$i\n\n";
+    print "mkdir replicate.\$i\n\ncp ../";
+    print join ("\ncp ../", @replicate_sorted), "\n\n";
 
 }
+
+
+__END__
+
+print statements will print something like:
+
+"i=0
+
+ mkdir replicate.$i
+ cp ../G15821_all.aln.trim.fas replicate.$i
+ cp ../G15823_all.aln.trim.fas replicate.$i
+ cp ../G15837_all.aln.trim2.fas replicate.$i
+ cp ../G15845_all.aln.trim2.fas replicate.$i
+ cp ../G15851_all.aln.trim.fas replicate.$i
+ cp ../G15858_all.aln.trim.fas replicate.$i
+ cp ../G15867_all.aln.trim.fas replicate.$i
+ cp ../G15869_all.aln.trim.fas replicate.$i
+
+
+i=1
+
+ mkdir replicate.$i
+ cp ../G15807_all.aln.trim.fas replicate.$i
+ cp ../G15820_all.aln.trim.fas replicate.$i
+ cp ../G15823_all.aln.trim.fas replicate.$i
+ cp ../G15832_all.aln.trim.fas replicate.$i
+ cp ../G15837_all.aln.trim2.fas replicate.$i
+ cp ../G15844_all.aln.trim.fas replicate.$i
+ cp ../G15845_all.aln.trim2.fas replicate.$i
+
+i=2
+
+etc"

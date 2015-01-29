5c47e09c51d97c667dd0296f4240fd30d8541732
iirisarri <irisarri.iker@gmail.com>
2015-01-29 11:40:17 +0100

    added comments and prints

--- blast_RBH_to_prot.pl
+++ blast_RBH_to_prot.pl
@@ -10,6 +10,7 @@ use Bio::DB::Fasta;
 # modified version of parse_blast_best_hit_prot.pl
 # it checks for reciprocal blast best hits
 # option to use a filter for an evalue threshold
+# IMPORTANT: It will output hits from $blast_report_1 (order in which reports are given is important!)
 # originally for getting comps of reciprocal best hits to extract from fasta files (transcriptomes)
 # translate them using the correct reading frame
 
@@ -17,22 +18,30 @@ use Bio::DB::Fasta;
 my $usage = "blast_RBH_to_prot.pl blast_report_1 blast_report_2 source_fasta > output.fa\n";
 my $blast_report_1 = $ARGV[0] or die $usage;
 my $blast_report_2 = $ARGV[1] or die $usage;
-#my $in_fasta = $ARGV[2] or die $usage;
+my $in_fasta = $ARGV[2] or die $usage;
 
 
 # process best hits from blast reports by sending it the subroutine (= blast_to_best_hit_prot.pl)
+print STDERR "\nprocessing blast report #1...\n";
+
 my $blast_1_ref = blast_to_hash($blast_report_1);
+
+print STDERR "processing blast report #2...\n";
 my $blast_2_ref = blast_to_hash($blast_report_2);
 
 # de-reference hashes
 my %blast_1 = %{ $blast_1_ref };
 my %blast_2 = %{ $blast_2_ref };
 
+# count original number of queries in blast report #1
+my $original_query_num = scalar keys %blast_1;
+
 #print Dumper \$blast_1_ref;
 #print Dumper \$blast_2_ref;
 
 my %RBBH_results;
 
+print STDERR "searching for RBBH...\n";
 
 foreach my $query_1 ( keys %blast_1 ) {
 
@@ -53,6 +62,13 @@ foreach my $query_1 ( keys %blast_1 ) {
 
 #print Dumper \%blast_1;
 
+# count number of queries in blast report #1 after deleting non-RBBH 
+my $query_num_after_delete = scalar keys %blast_1;
+my $percent = ($query_num_after_delete / $original_query_num ) * 100;
+
+print STDERR "original queries: $original_query_num\n";
+print STDERR "number of RBBHs: $query_num_after_delete ($percent %)\n";
+print STDERR "extracting RBBH from blast report #1 and translating into protein...\n";
 
 ###########################################
 # extract_from_fasta_by_name && translate #
@@ -60,7 +76,7 @@ foreach my $query_1 ( keys %blast_1 ) {
 
 # read fasta file with SeqIO
 my $seqio_obj = Bio::SeqIO->new('-file' => "<$in_fasta",
-                				'-format' => "fasta");
+         			'-format' => "fasta");
 
 my $prot_obj = Bio::SeqIO->new('-format' => "fasta");
 
@@ -76,11 +92,11 @@ while (my $seq_obj = $seqio_obj->next_seq){
     my $seqname = $seq_obj->primary_id;
     my $description = $seq_obj->description;
 
-    foreach my $key ( sort { $a cmp $b } keys %hash ) {
+    foreach my $key ( sort { $a cmp $b } keys %blast_1 ) {
 	# get best hits from source fasta
-	if ( $seqname eq $hash{$key}{'blast_info'}[0] ) {
+	if ( $seqname eq $blast_1{$key}{'blast_info'}[0] ) {
 	    # get all elements from @frames
-	    foreach my $fram ( @{ $hash{$key}{'frames'} } ) {
+	    foreach my $fram ( @{ $blast_1{$key}{'frames'} } ) {
 
 		# get "real" reading frame and substract 1 to convert to perl reading frames (0,1,2)
 		$fram =~ /-*(\d)/;
@@ -103,12 +119,14 @@ while (my $seq_obj = $seqio_obj->next_seq){
 
 		print STDOUT ">", $header_out, "\n";
 		print STDOUT $prot_obj->seq, "\n";
-		# scape foreach loop of %hash once the sequence is found
+		# scape foreach loop of %blast_1 once the sequence is found
 		next;
 	    }
 	}
     }
 }
+
+print STDERR "\ndone!\n\n";
 
 ### SUBROUTINES ###
 

3bd226cc32db5d45b81c0ce8fe7fe1b6f29e3f64
iirisarri <irisarri.iker@gmail.com>
2016-10-10 11:05:30 +0200

    cmd to skip empty seqs in infiles

--- gene_jackknife_from_fasta_collection.pl
+++ gene_jackknife_from_fasta_collection.pl
@@ -86,6 +86,9 @@ for (my $i=0; $i<$num_replicates; $i++) {
 		# store sequences of new file into hash %new
 		while (my $inseq = $seqio_obj->next_seq) {
 
+			# scape empty sequences in present
+			next if ( $inseq->seq =~ /^[-XNn?]*$/ );
+
 			$new{$inseq->primary_id}=$inseq->seq;
 			$new_seq_length = $inseq->length;
 		}

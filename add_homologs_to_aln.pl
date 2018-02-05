d2d1dc68beff8ab2359d463095327e3ea78ebaec
iirisarri <irisarri.iker@gmail.com>
2018-02-05 12:17:20 +0100

    bug fix with seq extraction

--- add_homologs_to_aln.pl
+++ add_homologs_to_aln.pl
@@ -17,7 +17,7 @@
 # Fasta parsing in subroutine
 #	-Alignment: it will de-align it to *.nogaps for BLAST
 #	-Database (transcriptome): it will make headers unique (eg. TransDecoder can predict 
-#	multiple ORFs per transcript). It will append seq length and "***" to headers
+#	multiple ORFs per transcript).
 #
 # In addition to main output file, the script will create intermediate files that will be
 #	retained for later use in "for" loops (*.nogaps, *.flags,blast DB to be removed manually)
@@ -42,7 +42,7 @@ my $alignment = $ARGV[0] or die $usage;
 my $database = $ARGV[1] or die $usage;
 my $num_hits = "2"; # max number of final hits to retain per database
 my $evalue = "1e-6"; # e-value threshold for BLASTP
-my $threads = "4"; # number of threads for BLASTP
+my $threads = "6"; # number of threads for BLASTP
 
 
 # 1. Input files
@@ -50,24 +50,22 @@ my $threads = "4"; # number of threads for BLASTP
 # ALIGNMENT
 my $alignment_unaligned = $alignment . ".nogaps";
 
-# check if unaligned file exists already (-z checks if file has 0 size)
-if ( !-z "$alignment_unaligned") {
+# check if unaligned file exists already (-e checks if file exists)
+if ( !-e "$alignment_unaligned") {
 
 	system("sed '/>/! s/-//g' $alignment > $alignment_unaligned");
 }
 
 # DATABASE (TRANSCRIPTOME)
-# check if unaligned file exists already
 my $database_flags = $database . ".flags";
-my %database;
 
-if ( !-z "$database_flags") {
+my	%database = read_fasta($database_flags);
 
-	# reformat alignment by removing gaps and adding flags to header (also made unique)
-	print STDERR "\nReformating input alignment: $alignment\nrm gaps and add flags to unique headers...\n";
+if ( !-e "$database_flags") {
 
-	%database = read_fasta($database);
-
+	# reformat alignment: rm gaps and make seq unique
+	print STDERR "\nReformating input alignment: $alignment\nrm gaps and make headers unique...\n";
+	
 	open(DB, ">", $database_flags) or die "Can't create $database_flags: $!";
 
 	foreach my $d ( sort keys %database ) {
@@ -81,7 +79,8 @@ if ( !-z "$database_flags") {
 # 2. blast each sequence in alignment against database
 
 # check if blast db already exists, otherwise create
-if ( !-z "$database_flags.phr") {
+if ( !-e "$database_flags.phr" && !-e "$database_flags.pin" && !-e "$database_flags.psd" && 
+     !-e "$database_flags.pog" && !-e "$database_flags.psi" && !-e "$database_flags.psq" ) {
 
 	print STDERR "\nCreating BLASTP database from: $database_flags...\n";
 	system("makeblastdb -in $database_flags -dbtype prot -parse_seqids");
@@ -92,14 +91,20 @@ else {
 }
 
 # blastp
-print STDERR "\nRunning BLASTP...\n";
 
 my $blast_out = $alignment_unaligned . "_vs_" . $database_flags . ".blastp";
 
-system("blastp -query $alignment_unaligned -db $database_flags -evalue $evalue -num_descriptions 1 -num_alignments 1 -num_threads $threads -out $blast_out");
+if ( !-e $blast_out ) {
+    
+    print STDERR "\nRunning BLASTP...\n";
+    system("blastp -query $alignment_unaligned -db $database_flags -evalue $evalue -num_descriptions 1 -num_alignments 1 -num_threads $threads -out $blast_out");
+}
+else {
+    print STDERR "\nBLASTP results already exist for: $blast_out...\n";
+}
 
-# 3. parse blast report (save x best hits)
-print STDERR "\nParsing BLASTP, retained $num_hits hits...\n";
+# 3. parse blast report (save N best hits)
+print STDERR "\nParsing BLASTP, retaining max. $num_hits hits...\n";
 
 my $report = new Bio::SearchIO(	-format => "blast", 
 				-file   => "<$blast_out",
@@ -148,14 +153,14 @@ print STDERR "\nExtracting best hits from: $database_flags\nOutput appended to: 
 open(OUT, ">>", $out_hits) or die "Can't create $out_hits: $!\n'";
 
 foreach my $h ( keys %hits ) {
-	
+    
 	if ( exists $database{$h} ) {
 		
 		print OUT ">$h\n";
 		print OUT "$database{$h}\n";
 	}
 }
-close(OUT);
+#close(OUT);
 
 
 # these three files can be reused if not removed every time
@@ -166,8 +171,9 @@ close(OUT);
 # when running pipeline for single species, we can add new sequences into the alignment and
 # re-align with mafft. When doing it for multiple species, it is easier to create one file
 # per alignment where we add all hits in fasta, later to be added to the alignment
+
 =pod
-# add to alignment
+# add hits back to alignment
 my $new_alignment = $alignment . ".hits";
 my $new_mafft = $new_alignment . ".mafft";
 
@@ -201,28 +207,22 @@ sub read_fasta {
 
 		my $seqname = $seq_obj->primary_id;
 		my $sequence = $seq_obj->seq;
-		my $length = $seq_obj->length;
-		
-		# add sequence length and "***" to new sequences
-		my $seqname_with_flag = $seqname . "-len=$length-***";
-		
+				
 		# remove gaps to sequence, if present
 		if ( $sequence =~ /\w*[-]+\w*/ ) {
 
 			$sequence =~ s/-//g;
 		}
 		# rename if duplicated headers are left (only if two sequences have same length)
-    	if ( !exists $seq_hash{$seqname_with_flag} ) {
+    	if ( !exists $seq_hash{$seqname} ) {
 
-			$seq_hash{$seqname_with_flag} = $sequence;
+			$seq_hash{$seqname} = $sequence;
 	    }
 		else {
 	
-        $repeated++;
-
-		my $new_seqname = $seqname_with_flag . "_" . $repeated;
-
-		$seq_hash{$new_seqname}	= $sequence;
+		    $repeated++;
+		    my $new_seqname = $seqname . "_" . $repeated;
+		    $seq_hash{$new_seqname}	= $sequence;
 		}
 	}
 	return %seq_hash;
